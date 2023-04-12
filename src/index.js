a39c8a0a9b0a54143f3d52c2b7e79ed315b3a78b
Matheus Fernandes Fermiano <matheus09fernandess@gmail.com>
2023-04-12 11:08:48 -0300

    feat: added eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import Login from './containers/Login';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>Code Burger</h1>
-);
+root.render(<Login />);
