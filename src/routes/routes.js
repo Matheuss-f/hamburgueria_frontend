307857db62fbcefbd6b673a9b3400c28f9b93aa8
Matheus Fernandes Fermiano <matheus09fernandess@gmail.com>
2023-04-14 11:17:40 -0300

    feat: initialized private routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
+import Home from '../containers/Home';
 import Login from '../containers/Login';
 import Register from '../containers/Register';
+import PrivateRoutes from './private-routes';
 
 function Routes() {
   return (
@@ -10,6 +12,7 @@ function Routes() {
       <Switch>
         <Route component={Login} path="/login" />
         <Route component={Register} path="/cadastro" />
+        <PrivateRoutes exact component={Home} path="/" />
       </Switch>
     </Router>
   );
