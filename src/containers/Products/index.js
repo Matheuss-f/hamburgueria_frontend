316af29b096d6d2de7aaa30a77fe378fdc0e7888
Matheus Fernandes Fermiano <matheus09fernandess@gmail.com>
2023-05-20 14:54:58 -0300

    feat: added cardproducts and filtered products

--- src/containers/Products/index.js
+++ src/containers/Products/index.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
 import ProductsLogo from '../../assets/products-logo.svg';
+import CardProduct from '../../components/CardProducts';
 import apiCodeBurguer from '../../services/api';
+import formatCurrency from '../../utils/formatCurrency';
 import {
   Container,
   ProductsImg,
   CategoryButton,
-  CategoriesMenu
+  CategoriesMenu,
+  ProductsContainer
 } from './style';
 
 function Products() {
   const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [activeCategory, setActiveCategory] = useState(0);
 
   useEffect(() => {
@@ -22,6 +27,32 @@ function Products() {
     }
     loadCategories();
   }, []);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const { data: allProducts } = await apiCodeBurguer.get('products');
+
+      const newProducts = allProducts.map(product => {
+        return { ...product, formatedPrice: formatCurrency(product.price) };
+      });
+
+      setProducts(newProducts);
+    }
+    loadProducts();
+  }, []);
+
+  useEffect(() => {
+    if (activeCategory === 0) {
+      setFilteredProducts(products);
+    } else {
+      const newFilteredProducts = products.filter(
+        product => product.category_id === activeCategory
+      );
+
+      setFilteredProducts(newFilteredProducts);
+    }
+  }, [activeCategory, products]);
+
   return (
     <Container>
       <ProductsImg src={ProductsLogo} alt="logo-home" />
@@ -40,6 +71,12 @@ function Products() {
             </CategoryButton>
           ))}
       </CategoriesMenu>
+      <ProductsContainer>
+        {filteredProducts &&
+          filteredProducts.map(product => (
+            <CardProduct key={product.id} product={product} />
+          ))}
+      </ProductsContainer>
     </Container>
   );
 }
--- src/containers/Products/style.js
+++ src/containers/Products/style.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: #e5e5e5;
+  min-height: 100vh;
+`;
 
 export const ProductsImg = styled.img`
   width: 100%;
@@ -22,4 +25,13 @@ export const CategoryButton = styled.button`
   line-height: 20px;
   background-color: transparent;
   padding-bottom: 5px;
+`;
+
+export const ProductsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  padding: 40px;
+  justify-items: center;
+  margin-top: 20px;
 `;
